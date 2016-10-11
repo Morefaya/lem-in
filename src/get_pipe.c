0d9647983ae978bb53e7ba218be7f13d8a68f9bd
Morefaya <jeancharles.cazako@gmail.com>
2016-10-11 02:47:29 +0200

    update get_pipe.c

--- src/get_pipe.c
+++ src/get_pipe.c
@@ -1,10 +1,52 @@
 #include "lem_in.h"
 
-t_list	*get_pipe(char *line)
+static int	get_name_1(char *line, t_pipe *pipe)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (line[i] && ft_strchr(" \t", line[i]))
+		i++;
+	while (line[i + j] && ft_strchr(" \t", line[i + j]))
+		j++;
+	if (!(pipe->n_1 = ft_strsub(line, i, j)))
+		return (-1);
+	return (i + j);
+}
+
+static int	get_name_2(char *line, t_pipe *pipe, int i)
+{
+	int	j;
+
+	j = 0;
+	while (line[i] && ft_strchr(" \t", line[i]))
+		i++;
+	if (line[i] == '-')
+		i++;
+	while (line[i] && ft_strchr(" \t", line[i]))
+		i++;
+	while (line[i + j] && ft_strchr(" \t", line[i + j]))
+		j++;
+	if (!(pipe->n_2 = ft_strsub(line, i, j)))
+		return (-1);
+	return (i + j);
+}
+
+t_list		*get_pipe(char *line)
 {
 	t_list	*p_lst;
 	t_pipe	content;
 	int	i;
 
-	get_name_1(line, &content, &i);
+	if ((i = get_name_1(line, &content)) == -1)
+		return (NULL);
+	else if (get_name_2(line, &content, i) == -1)
+	{
+		free(content.n_1);
+		return (NULL);
+	}
+	
+	
 }
