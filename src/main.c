618869bbe1bae117ab41ec791d26eee1e8f71d59
Morefaya <jeancharles.cazako@gmail.com>
2016-10-23 02:26:18 +0200

    update for algo ant

--- src/main.c
+++ src/main.c
@@ -12,7 +12,68 @@
 
 #include "lem_in.h"
 
-int	main(void)
+static int	count_pipe(t_list *h_lst)
+{
+	t_list	*xion;
+	int	ret;
+
+	ret = 0;
+	xion = ((t_hill*)(h_lst->content))->xion;
+	while (xion)
+	{
+		ret++;
+		xion = xion->next;
+	}
+	return (ret);
+}
+
+static int	get_max_queue(t_list *h_lst)
+{
+	int	cmd;
+	int	nb_pipe_1;
+	int	nb_pipe_2;
+
+	nb_pipe_1 = 0;
+	nb_pipe_2 = 0;
+	while (h_lst)
+	{
+		cmd = ((t_hill*)(h_lst->content))->cmd;
+		if (cmd == START)
+			nb_pipe_1 = count_pipe(h_lst);
+		else if (cmd == END)
+			nb_pipe_2 = count_pipe(h_lst);
+		h_lst = h_lst->next;
+	}
+	if (nb_pipe_1 < nb_pipe_2)
+		return (nb_pipe_1);
+	else
+		return (nb_pipe_2);
+}
+
+static int	check_start_end(t_list *r_lst)
+{
+	int	start;
+	int	end;
+	int	cmd;
+
+	start = 0;
+	end = 0;
+	while (r_lst)
+	{
+		cmd = ((t_room*)(r_lst->content))->cmd;
+		if (cmd == START)
+			start = 1;
+		else if (cmd == END)
+			end = 1;
+		r_lst = r_lst->next;
+	}
+	if (start && end)
+		return (0);
+	else
+		return (1);
+}
+
+int		main(void)
 {
 	t_list	*line_lst;
 	int	nb_ant;
@@ -24,7 +85,7 @@ int	main(void)
 	t_list	*a_lst;
 	t_list	*w_lst;
 	t_list	*ph_lst;
-	t_list	*as_lst;
+	int	max_queue;
 
 	if (!(line_lst = get_anthill()))
 		return (1);
@@ -37,7 +98,7 @@ int	main(void)
 	}
 	ft_lstrotate(&line_lst);
 	i = 1;
-	if (!(r_lst = mk_lstroom(&line_lst)))
+	if (!(r_lst = mk_lstroom(&line_lst)) || check_start_end(r_lst))
 	{
 		ft_lstdel(&line_lst, (void(*)(void*, size_t))del_line);
 		ft_printf("Error_2\n");
@@ -54,11 +115,12 @@ int	main(void)
 	a_lst = get_antlst(nb_ant, h_lst);
 	init_ant(a_lst, h_lst);
 	init_xion(&h_lst, p_lst);
+	max_queue = get_max_queue(h_lst);
 	w_lst = NULL;
 	ph_lst = NULL;
-	as_lst = NULL;
 	path_finder(h_lst, &w_lst, &ph_lst);
 	print_path(ph_lst);
+	ft_printf("max_queue :%d\n", max_queue);
 	ft_lstdel(&w_lst, (void(*)(void*, size_t))del_xion);
 	ft_lstdel(&r_lst, (void(*)(void*, size_t))del_room);
 	ft_lstdel(&p_lst, (void(*)(void*, size_t))del_pipe);
